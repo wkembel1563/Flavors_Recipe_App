4e7b95e7f83c8dccc993b672c7d78fc043cd6d79
wkembel1563 <robert.kembel@mavs.uta.edu>
2021-10-02 16:53:30 -0500

    test push

--- app/src/main/java/com/example/flavors_prototype/MainActivity.kt
+++ app/src/main/java/com/example/flavors_prototype/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    // test
 
     lateinit var dataBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
